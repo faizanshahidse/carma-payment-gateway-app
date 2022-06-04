553f0c76c31343c70f33b7ff0bc13e8e25312073
divsync-faizans <faizans@divsync.com>
2022-06-04 17:50:19 +0500

    vite reactjs configuration added

--- server/controllers/payments.js
+++ server/controllers/payments.js
@@ -2,7 +2,10 @@ import asyncHandler from 'express-async-handler';
 
 const addCreditCardDetails = asyncHandler((req, res) => {
   console.log('credit card details....');
-  res.json({ message: 'success' });
+
+  const { cardNo, cvv, cardHolderName, expiryDate } = req.body;
+
+  res.json({ message: 'congratulations!' });
 });
 
 export { addCreditCardDetails };
