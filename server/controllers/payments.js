a9b195eb500b4e9d59c897c5b56b5f751cbe55a2
divsync-faizans <faizans@divsync.com>
2022-06-04 13:30:09 +0500

    modify babel config

--- server/controllers/payments.js
+++ server/controllers/payments.js
@@ -1,7 +1,8 @@
 import asyncHandler from 'express-async-handler';
 
-const addCreditCardDetails = asyncHandler(() => {
+const addCreditCardDetails = asyncHandler((req, res) => {
   console.log('credit card details....');
+  res.json({ message: 'success' });
 });
 
 export { addCreditCardDetails };
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,6 @@ import payment from './payments';
 
 const app = express();
 
-console.log('helo 1...........');
-
 app.use('/card', payment);
 
 export default app;
