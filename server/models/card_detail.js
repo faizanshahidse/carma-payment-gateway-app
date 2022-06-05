855825a1d71ab5422688de6994006fd75553f3e0
divsync-faizans <faizans@divsync.com>
2022-06-05 15:24:48 +0500

    cardNo, cvv encryption added

--- server/models/card_detail.js
+++ server/models/card_detail.js
@@ -1,5 +1,7 @@
 'use strict';
 import { Model } from 'sequelize';
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
   class card_detail extends Model {
     /**
@@ -13,8 +15,8 @@ module.exports = (sequelize, DataTypes) => {
   }
   card_detail.init(
     {
-      cardNo: { allowNull: false, unique: true, type: DataTypes.STRING },
-      cvv: { allowNull: false, type: DataTypes.STRING },
+      cardNo: { allowNull: false, unique: true, type: DataTypes.STRING(16) },
+      cvv: { allowNull: false, type: DataTypes.STRING(3) },
       cardHolderName: { allowNull: false, type: DataTypes.STRING },
       expiryDate: { allowNull: false, type: DataTypes.DATE },
     },
@@ -23,5 +25,11 @@ module.exports = (sequelize, DataTypes) => {
       modelName: 'card_detail',
     },
   );
+
+  card_detail.beforeCreate(async (card) => {
+    card.cardNo = await bcrypt.hash(card.cardNo, 12);
+    card.cvv = await bcrypt.hash(card.cvv, 12);
+  });
+
   return card_detail;
 };
